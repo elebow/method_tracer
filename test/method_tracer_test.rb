9a9f407cb2e1ed8d192224fbee3e3fc47fde8bfc
Eddie Lebow <elebow@users.noreply.github.com>
2018-09-26 00:01:21 -0400

    Short-circuit based on classname, if provided
    Something (probably the Where.is call) is extremely slow, so allowing
    the user to provide a classname of interest lets us short-circuit and
    save some time.
    
    For ease of testing, Tracer no longer creates TracePoints upon
    initialization. Use the new method #enable.

--- test/method_tracer_test.rb
+++ test/method_tracer_test.rb
@@ -10,16 +10,16 @@ class MethodTracerTest < Minitest::Test
     test_base_dir = __dir__
     MethodTracer::Config.app_path = test_base_dir + '/fixtures/my-app'
     MethodTracer::Config.output_file = StringIO.new
-    MethodTracer::Tracer.new(test_base_dir + '/fixtures/my-great-gem')
+    MethodTracer::Tracer.new(path: test_base_dir + '/fixtures/my-great-gem').enable
 
     App.new.run_instance_method
     App.new.run_class_method
 
     report_lines = MethodTracer::Config.output_file.string
     expected_lines = %r{
-      #{Regexp.quote(test_base_dir)}/fixtures/my-great-gem\ :great_instance_method\n
+      MyGreatGem\ :great_instance_method\n
       #{Regexp.quote(test_base_dir)}/fixtures/my-app/app.rb:5\n
-      #{Regexp.quote(test_base_dir)}/fixtures/my-great-gem\ :great_class_method\n
+      \#<Class:MyGreatGem>\ :great_class_method\n
       #{Regexp.quote(test_base_dir)}/fixtures/my-app/app.rb:9\n
     }x
     assert expected_lines.match(report_lines)
@@ -27,5 +27,14 @@ class MethodTracerTest < Minitest::Test
     # assert the original methods were executed
     assert_equal MyGreatGem.instance_variable_get(:@instance_method_executed), 'great!'
     assert_equal MyGreatGem.instance_variable_get(:@class_method_executed), 'great!!!!!!!'
+  end
+
+  def test_short_circuit_on_class_name
+    tracer = MethodTracer::Tracer.new(path: "#{__dir__}/fixtures/my-great-gem", name: 'OtherGem')
+
+    # We want to assert that the short circuit is taken, so make a later method raise an exception
+    tracer.define_singleton_method(:method_is_defined_in_target_path, proc { raise })
+
+    assert !tracer.method_is_interesting?(MyGreatGem, :great_method)
   end
 end
